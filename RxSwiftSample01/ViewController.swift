8303d4b2b5ab94ba465cab1e93778c952adfadce
Shizuna Ito <adfa1728@chiba-u.jp>
2018-01-08 16:40:10 +0900

    set up UI

--- RxSwiftSample01/ViewController.swift
+++ RxSwiftSample01/ViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    enum State: Int {
+        case useButtons
+        case useTextField
+    }
+
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var stateSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var greetingsTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+
+    @IBOutlet var greetingButtons: [UIButton]!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
